be2ac7fe15c4adb096cf335a0367f7794acaca40
Naresh Chidipothu <nchidipo@meiobee.com>
2016-01-24 10:59:23 +0530

    first commit

---
+++ src/main/java/com/meiobee/nchidi/App.java
@@ -0,0 +1,13 @@
+package com.meiobee.nchidi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello kljlkj World!" );
+    }
+}
